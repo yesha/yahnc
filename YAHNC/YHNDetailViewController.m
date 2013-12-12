9ff56fd17183c62846009c9625261e4ee125e7e2
Daniel Ge <dange@seas.upenn.edu>
2013-12-11 23:51:10 -0500

    Resolve string format warnings thrown by Xcode

--- YAHNC/YHNDetailViewController.m
+++ YAHNC/YHNDetailViewController.m
@@ -44,11 +44,11 @@
         self.detailDescriptionLabel.text = [self.detailItem description];
         
         self.postTitle.text   = [self.detailItem title];
-        self.postVoteNum.text = [NSString stringWithFormat:@"%i",
-                                 [self.detailItem score]];
+        self.postVoteNum.text = [NSString stringWithFormat:@"%li",
+                                 (long)[self.detailItem score]];
         // TODO timestamp
-        self.postCommentNum.text = [NSString stringWithFormat:@"%i comments",
-                                    [self.detailItem commentCount]];
+        self.postCommentNum.text = [NSString stringWithFormat:@"%li comments",
+                                    (long)[self.detailItem commentCount]];
         // TODO link?
     }
 }
